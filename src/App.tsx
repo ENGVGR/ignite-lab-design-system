a6194ee5edae294a645e8bd343ea6fcaafacd6e5
Vinícius Ribeiro <vgrgonzaga@outlook.com>
2022-10-16 22:08:02 -0300

    Crete page and tests

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+import { Heading } from "./componets/Heading";
+import { Text } from "./componets/Text";
+import { TextInput } from "./componets/TextInput";
+import Logo from "./Logo";
 import "./styles/global.css";
+import Envelope from "./assets/EnvelopeSimple.png";
+import Cadeado from "./assets/Lock.png";
+import { Checkbox } from "./componets/checkbox";
+import { Button } from "./componets/Button";
+import { SignIn } from "./pages/Signin";
 
 export function App() {
-  return (
-    <>
-      <h1 className="font-bold text-xs text-cyan-300">Hello World!</h1>
-    </>
-  );
+  return <SignIn />;
 }
